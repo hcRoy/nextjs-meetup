d3a09b150c444bf13613f0d0f732a29067163ed5
hcRoy <harshchanchpara9999@gmail.com>
2023-02-16 14:42:41 +0530

    ready for deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://harsh-roy:hH123456@nextjs-cluster.lji3llb.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Data Inserted!" });
+  }
+}
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,32 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+export default function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    router.push("/");
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new meetup</title>
+        <meta
+          name="description"
+          content="Create your own meetup and organize them"
+        ></meta>
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
+    </Fragment>
+  );
+}
